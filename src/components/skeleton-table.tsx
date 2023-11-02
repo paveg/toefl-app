a416636a7feb3ce601d8fbeffe5b103ae707ae7f
Ryota Ikezawa <pavegy@gmail.com>
2023-11-02 22:56:54 +0900

    :recycle: Add skeleton table

---
+++ src/components/skeleton-table.tsx
@@ -0,0 +1,55 @@
+import { type FC } from "react";
+import { Skeleton } from "~/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/ui/table";
+
+type Props = {
+  tableHeader?: React.ReactNode;
+  columnCount?: number;
+};
+
+export const SkeletonTable: FC<Props> = ({
+  tableHeader,
+  columnCount,
+}: Props) => {
+  return (
+    <Table className="mb-4 table-auto rounded-md">
+      {tableHeader ? (
+        tableHeader
+      ) : (
+        <TableHeader>
+          <TableRow>
+            {Array<number>(columnCount).fill(0).map((_, i) => (
+              <TableHead
+                key={`skeleton-table-head-${i}`}
+                className="text-center"
+              >
+                <Skeleton className="h-4 w-full md:h-6" />
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+      )}
+      <TableBody>
+        {[...Array<number>(10)].map((_, i) => (
+          <TableRow key={`skeleton-table-row-${i}`}>
+            {Array<number>(columnCount).fill(0).map((_, i) => (
+              <TableCell
+                key={`skeleton-table-cell-${i}`}
+                className="text-center"
+              >
+                <Skeleton className="h-4 w-full md:h-6" />
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
--- src/components/words/container.tsx
+++ src/components/words/container.tsx
@@ -1,12 +1,23 @@
 import { api } from "~/utils/api"
 import { WordTable } from "./table"
+import { SkeletonTable } from "../skeleton-table"
+import { TableHead, TableHeader, TableRow } from "~/ui/table"
 
 export const WordContainer = () => {
   const { data: words, isLoading } = api.word.getAll.useQuery()
 
   return (
     <>
-      {isLoading ? (<div>Loading...</div>) : (words?.length && <WordTable words={words} />)}
+      {isLoading ? (<SkeletonTable tableHeader={
+        <TableHeader>
+          <TableRow>
+            <TableHead className="">単語</TableHead>
+            <TableHead className="">意味</TableHead>
+            <TableHead className="">発音記号</TableHead>
+            <TableHead className="">品詞</TableHead>
+          </TableRow>
+        </TableHeader>
+      } columnCount={4} />) : (words?.length && <WordTable words={words} />)}
     </>
   )
 }
