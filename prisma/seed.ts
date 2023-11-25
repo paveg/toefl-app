93ef66fdfd96b1d18e22b4f214c85320d495f87a
Ryota Ikezawa <pavegy@gmail.com>
2023-11-25 12:46:19 +0900

    :recycle: Update seed logic

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from '@prisma/client'
-import { wordList } from './wordList'
+import { wordList } from './wordList';
 const prisma = new PrismaClient()
 async function main() {
-  const words = await prisma.word.createMany({
-    data: wordList
+  const existingWords = await prisma.word.findMany({
+    where: {
+      word: {
+        in: wordList.map((word) => word.word)
+      }
+    }
+  })
+  const existingList = existingWords.map((word) => word.word)
+  const newWords = wordList.filter((word) => {
+    return !existingList.includes(word.word)
+  })
+  await prisma.word.createMany({
+    data: newWords
   })
 }
 
